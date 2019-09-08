c159cd24e0efadc4edbad035045bcb0a9d072a27
tunche1231 <53240990+tunche1231@users.noreply.github.com>
2019-09-08 12:35:00 -0500

    mmm

--- app/src/main/java/com/example/miprimeraapp/Main3Activity.java
+++ app/src/main/java/com/example/miprimeraapp/Main3Activity.java
@@ -2,13 +2,38 @@ package com.example.miprimeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Main3Activity extends AppCompatActivity {
+    Button btn;
+    LinearLayout line;
+    int contar = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        btn = findViewById(R.id.btn_contador);
+        line = findViewById(R.id.lycontador);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView a = new TextView(getApplicationContext());
+                a.setText("Hola"+contar);
+                contar++;
+                if(contar == 10){
+                    line.addView(a);
+                    Toast.makeText(Main3Activity.this,"Holas",Toast.LENGTH_LONG);
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/miprimeraapp/Main6Activity.java
+++ app/src/main/java/com/example/miprimeraapp/Main6Activity.java
@@ -1,15 +1,14 @@
 package com.example.miprimeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class Main5Activity extends AppCompatActivity {
+public class Main6Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
-
+        setContentView(R.layout.activity_main6);
     }
-
 }
